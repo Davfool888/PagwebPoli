c512b613a2d3e7833da7ed4c265c997659a6e0d5
Davfool888 <davfool888@gmail.com>
2023-06-13 10:50:49 -0500

    Segundo avance de la pag web, incluye interaccion

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-bikes', {delay: 500});
+ScrollReveal().reveal('.bikes-banner-one', {delay: 500});
+ScrollReveal().reveal('.bikes-banner-two', {delay: 500});
+
